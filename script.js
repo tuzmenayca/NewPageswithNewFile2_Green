9e600c23c48e0eeab032cf143afde5872f9effbb
AycaTuzmen <9531002-AycaTuzmen@users.noreply.replit.com>
2023-04-20 21:10:16 +0000

    added new help button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var backButton
+var helpButton
 
 function setup() {
   createCanvas(400, 400);
@@ -9,8 +10,15 @@ function draw() {
   backButton = createButton("Back");
   backButton.position(350,350)
   backButton.mousePressed(clickedBack);
+  helpButton = createButton("Help");
+  helpButton.position(150,150)
+  helpButton.mousePressed(help);
+  
 }
 
 function clickedBack(){
     window.open("https://NewPageswithNewFile1.aycatuzmen.repl.co", "_self");
+}
+function help(){
+    print("click on hepl")
 }
