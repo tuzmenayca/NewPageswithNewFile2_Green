d557501c42defbb2c00883649835dc202026699e
tuzmenayca <146239123+tuzmenayca@users.noreply.github.com>
2024-09-26 07:59:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function clickedBack(){
     //window.open("https://NewPageswithNewFile1.aycatuzmen.repl.co", "_self");
 //window.open("https://95dc9fa3-111d-4f01-87bc-9fa7119dca16-00-1j3oyk5cngcsm.riker.repl.co/","_self");
   //window.open("https://4c9b4082-9814-46a5-a26f-bb8c4b55b56d-00-g1bchx78csoz.worf.repl.co/","_self");
-  window.open ("https://replit.com/@AycaTuzmen/NewPageswithNewFile1#script.js", "_self");
+  window.open ("https://tuzmenayca.github.io/NewPageswithNewFile2_Green/", "_self");
   //window.open ("https://4c9b4082-9814-46a5-a26f-bb8c4b55b56d-00-g1bchx78csoz.worf.replit.dev/", "_self");
  // window.open("https://replit.com/@pgray13/DementiaDestroyersApp", "_self");
   
