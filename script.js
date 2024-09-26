e4939564d5e9f2c847ad9775468295456673f352
tuzmenayca <146239123+tuzmenayca@users.noreply.github.com>
2024-09-26 14:54:08 +0000

    Update url

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var backButton
 var helpButton
 
+
 function setup() {
   createCanvas(400, 400);
-}
-
-function draw() {
   background("green")
   backButton = createButton("Back");
   backButton.position(350,350)
@@ -13,12 +11,22 @@ function draw() {
   helpButton = createButton("Help");
   helpButton.position(150,150)
   helpButton.mousePressed(help);
+}
+
+function draw() {
+
   
 }
 
 function clickedBack(){
-    window.open("https://NewPageswithNewFile1.aycatuzmen.repl.co", "_self");
+    //window.open("https://NewPageswithNewFile1.aycatuzmen.repl.co", "_self");
+//window.open("https://95dc9fa3-111d-4f01-87bc-9fa7119dca16-00-1j3oyk5cngcsm.riker.repl.co/","_self");
+  //window.open("https://4c9b4082-9814-46a5-a26f-bb8c4b55b56d-00-g1bchx78csoz.worf.repl.co/","_self");
+  window.open ("https://replit.com/@AycaTuzmen/NewPageswithNewFile1#script.js", "_self");
+  //window.open ("https://4c9b4082-9814-46a5-a26f-bb8c4b55b56d-00-g1bchx78csoz.worf.replit.dev/", "_self");
+ // window.open("https://replit.com/@pgray13/DementiaDestroyersApp", "_self");
+  
 }
 function help(){
-    print("click on hepl")
+    print("click on help")
 }
